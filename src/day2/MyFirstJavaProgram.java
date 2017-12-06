6cb18177fbb952225077dcdd4bb31fdd4fd02c3c
codeclub <codeclub@smms-rm23-36.smusd.local>
2017-12-06 14:58:23 -0800

    Spiral

--- src/day2/MyFirstJavaProgram.java
+++ src/day2/MyFirstJavaProgram.java
@@ -8,8 +8,14 @@ public class MyFirstJavaProgram {
 	public static void main(String[] args) {
 		
 		// START HERE
-	
-		
-		
+	Robot Sausage = new Robot ();
+	Sausage.setSpeed(100);
+	Sausage.penDown();
+	Sausage.setPenWidth(20);
+	Sausage.setAngle(500);
+	for(int i + 0; i < 4; i++) {
+	Sausage.move(250);	
+	Sausage.turn(130);
+	}
 	}
 }
